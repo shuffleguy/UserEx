d161cc4868614afd803d05af9c1ad2d3e6d90fc7
lvinicio <lucas.vinicius.oliveira@everis.com>
2021-03-27 14:25:00 -0300

    Criação da adapter

---
+++ app/src/main/java/lucas/oliveira/myapplication/ContactAdapter.kt
@@ -0,0 +1,8 @@
+package lucas.oliveira.myapplication
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter :RecyclerView.Adapter<> {//herda o recyclerview
+    
+}
+// classe que vai implementar os componentes do reclycer
--- app/src/main/java/lucas/oliveira/myapplication/MainActivity.kt
+++ app/src/main/java/lucas/oliveira/myapplication/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //precisa de uma classe para atuar com os itens e outra para regar
+        //criar a adapter
     }
 }
